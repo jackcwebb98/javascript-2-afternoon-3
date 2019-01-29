f8b19c154a251318bb5a0449ba9b471bc5ae81b4
Jack Webb <jackwebb@Jacks-MacBook-Pro.local>
2019-01-29 15:56:41 -0700

    finished assignment

--- practice.js
+++ practice.js
@@ -28,6 +28,9 @@
 */
 
 // Code Here 
+function first(arr, cb){
+  cb(arr[0])
+}
 
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -48,6 +51,9 @@ first(names, function(firstName){
 */
 
 //Code Here
+function last(arr, cb){
+  cb(arr[arr.length-1])
+}
 
 // Do not edit the code below.
 last(names, function(lastName){
@@ -66,6 +72,9 @@ last(names, function(lastName){
 */
 
 //Code Here
+function multiply(num1, num2, cb){
+  cb(num1 * num2)
+}
 
 // Do not edit the code below.
 multiply(4, 3, function(answer){
@@ -85,6 +94,18 @@ multiply(4, 3, function(answer){
 */
 
 //Code Here 
+function contains(arr, name, cb){
+  for (var i = 0; i <= arr.length; i++){
+    if (arr[i] === name){
+      cb(true)
+    }
+    else {
+      cb(false)
+    }
+  }
+}
+
+
 
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
@@ -106,6 +127,17 @@ contains(names, 'Colt', function(result){
 */
 
 //Code Here
+function uniq(arr, cb){
+  var modArr = []
+  for (var i = 0; i < arr.length; i++){
+    if(modArr.indexOf(arr[i]) === -1){
+      modArr.push(arr[i])
+    }
+  }
+  cb(modArr)
+}
+
+
 
 // Do not edit the code below.
 uniq(names, function(uniqArr){
@@ -123,6 +155,11 @@ uniq(names, function(uniqArr){
 */
 
 //Code Here 
+function each(arr, cb){
+  for (var i = 0; i < arr.length; i++){
+    cb(arr[i], arr.indexOf(arr[i]))
+  }
+}
 
 // Do not edit the code below.
 each(names, function(item, indice){
@@ -140,6 +177,13 @@ each(names, function(item, indice){
 */
 
 // Code here
+function getUserById(users, id, callback){
+  for (var i = 0; i < users.length; i++){
+    if (users[i].id === id){
+        callback(users[i])
+    }
+  }
+}
 
 // Do not edit the code below.
 var users = [
